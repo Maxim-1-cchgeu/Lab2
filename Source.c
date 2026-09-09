01c02bae3711e56d18f236c5ce5665e2475039e2
Maxim-1-cchgeu <a190lmax@yandex.ru>
2026-09-09 23:11:26 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -39,7 +39,7 @@ void paperwalls()
 
 	float wall_area = wall_length * wall_height;
 	float roll_area = ROLL_LENGTH * ROLL_WIDTH;
-	int roll_quantity = (wall_area + roll_area - 1) / roll_area; // Деление с округлением через прибавление делителя и вычитание 1
+	int roll_quantity = (wall_area + roll_area - 1) / roll_area; // Деление с округлением до верхней границы
 	float total_price = roll_quantity * roll_price;
 	
 	printf("Обои для всей стены будут стоить: %6.2f руб.\n", total_price);
